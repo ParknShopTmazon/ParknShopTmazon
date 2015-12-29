c8a5cb5c8f403a79a48a0bc678c95d476b685050
Winston <zhengruin@126.com>
2015-12-29 22:30:38 +0800

    update

--- code/Parknshop/src/main/java/com/tmazon/servlet/ShowOrderServlet.java
+++ code/Parknshop/src/main/java/com/tmazon/servlet/ShowOrderServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class AddOrderServlet extends HttpServlet {
+public class ShowOrderServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
