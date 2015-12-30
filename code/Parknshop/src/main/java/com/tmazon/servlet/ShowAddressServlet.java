4f602317a707a7b6109f8534541dc315c2dddc37
13121221 <974940306@qq.com>
2015-12-30 15:55:31 +0800

    ShowAddressServlet.java

--- code/Parknshop/src/main/java/com/tmazon/servlet/ShowAddressServlet.java
+++ code/Parknshop/src/main/java/com/tmazon/servlet/ShowAddressServlet.java
@@ -6,21 +6,33 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-public class ShowOrderServlet extends HttpServlet {
+import com.tmazon.domain.User;
+import com.tmazon.util.AttrName;
+
+import net.sf.json.JSONObject;
+
+public class ShowAddressServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
+
+	
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		req.getRequestDispatcher("WEB-INF/customer/order.jsp").forward(req, resp);
+		
+		HttpSession session = req.getSession();
+		User user = (User) session.getAttribute(AttrName.SessionScope.USER);
+		JSONObject jsonObject = new JSONObject();
+		
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		doGet(req,resp);
+		doGet(req, resp);
 	}
-
+	
 	
 	
 }
