880e2f983125a8f05a71331c265a593d58dea02c
13121221 <974940306@qq.com>
2015-12-30 16:19:16 +0800

    test1.sql readme.txt

--- testdata_customer/update_customer.sql
+++
@@ -1,5 +0,0 @@
-UPDATE `parknshop`.`product` SET `picture`='images/product/78781.jpg' WHERE `productId`='78781';
-UPDATE `parknshop`.`product` SET `picture`='images/product/78782.jpg' WHERE `productId`='78782';
-UPDATE `parknshop`.`product` SET `picture`='images/product/78783.jpg' WHERE `productId`='78783';
-UPDATE `parknshop`.`product` SET `picture`='images/product/78784.jpg' WHERE `productId`='78784';
-UPDATE `parknshop`.`product` SET `picture`='images/product/78785.jpg' WHERE `productId`='78785';
