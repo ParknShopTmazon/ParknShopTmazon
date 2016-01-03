e285651b98c2712fd1d0b118c97ae5e164bc96cc
13121266 <withqong@qq.com>
2016-01-03 20:45:37 +0800

    change index page

--- code/Parknshop/src/main/java/com/tmazon/servlet/IndexServlet.java
+++ code/Parknshop/src/main/java/com/tmazon/servlet/IndexServlet.java
@@ -1,12 +1,15 @@
 package com.tmazon.servlet;
 
 import java.io.IOException;
+import java.util.Comparator;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.tmazon.domain.Product;
 import com.tmazon.service.ProductService;
 import com.tmazon.util.AttrName;
 import com.tmazon.util.BasicFactory;
@@ -25,8 +28,18 @@ public class IndexServlet extends HttpServlet {
 			throws ServletException, IOException {
 		
 		String[] categories = productService.findAllCategory();
-		
 		req.setAttribute(AttrName.RequestScope.CATEGORIES, categories);
+		
+		List<Product> products = productService.select(new Product(null, null, null, null, null, req.getParameter("category"), null, null, null, null, null));
+		products.sort(new Comparator<Product>() {
+
+			public int compare(Product o1, Product o2) {
+				return o2.getSoldNum() - o1.getSoldNum();
+			}
+			
+		});
+		req.setAttribute(AttrName.RequestScope.PRODUCTS, products);
+		
 		req.getRequestDispatcher("WEB-INF/customer/index.jsp").forward(req, resp);
 	}
 	
