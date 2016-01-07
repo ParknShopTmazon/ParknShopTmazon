05be7f7796761c4a1b66fd98c9426f08dfd41195
Winston <zhengruin@126.com>
2016-01-07 10:01:05 +0800

    Winston_Admin_Update

--- code/Parknshop/src/main/java/com/tmazon/servlet/UserManageAdminServlet.java
+++ code/Parknshop/src/main/java/com/tmazon/servlet/UserManageAdminServlet.java
@@ -6,19 +6,31 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.tmazon.domain.User;
+import com.tmazon.util.AttrName;
 
 public class UserManageAdminServlet extends HttpServlet{
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		super.doGet(req, resp);
+		HttpSession session = req.getSession(false);
+		User onlineUser = (User) session.getAttribute(AttrName.SessionScope.USER);
+		if(onlineUser == null || !onlineUser.getRole().equals(User.ROLE_ADMIN))
+		{
+			if (session != null) {
+				session.invalidate();
+			}
+			resp.sendRedirect("login");
+		}
+		else
+		{
+			req.getRequestDispatcher("WEB-INF/admin/userManagement.jsp").forward(req,resp);
+		}
 	}
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		super.doPost(req, resp);
-	}
+	
 
 }
