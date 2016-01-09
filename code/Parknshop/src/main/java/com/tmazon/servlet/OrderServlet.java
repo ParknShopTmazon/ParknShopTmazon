d611c69b7d1c8f932b1b8cd026e0715bfbd4d781
13121266 <withqong@qq.com>
2016-01-09 20:49:24 +0800

    change OrderServlet.java

--- code/Parknshop/src/main/java/com/tmazon/servlet/OrderServlet.java
+++ code/Parknshop/src/main/java/com/tmazon/servlet/OrderServlet.java
@@ -24,6 +24,7 @@ import com.tmazon.service.DeliveryService;
 import com.tmazon.service.OrderService;
 import com.tmazon.service.ProductService;
 import com.tmazon.service.ShopService;
+import com.tmazon.service.UserService;
 import com.tmazon.util.AttrName;
 import com.tmazon.util.BasicFactory;
 import com.tmazon.util.ParseUtil;
@@ -34,79 +35,157 @@ public class OrderServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	private OrderService orderService = BasicFactory.getImpl(OrderService.class);
-	private ProductService productService = BasicFactory.getImpl(ProductService.class);
-	private DeliveryService deliveryService = BasicFactory.getImpl(DeliveryService.class);
+
+	private OrderService orderService = BasicFactory
+			.getImpl(OrderService.class);
+	private ProductService productService = BasicFactory
+			.getImpl(ProductService.class);
+	private DeliveryService deliveryService = BasicFactory
+			.getImpl(DeliveryService.class);
 	private ShopService shopService = BasicFactory.getImpl(ShopService.class);
-	private AddressService addressService = BasicFactory.getImpl(AddressService.class);
-	
+	private AddressService addressService = BasicFactory
+			.getImpl(AddressService.class);
+	private UserService userService = BasicFactory.getImpl(UserService.class);
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
-		
-		User user = (User) req.getSession().getAttribute(AttrName.SessionScope.USER);
+
+		User user = (User) req.getSession().getAttribute(
+				AttrName.SessionScope.USER);
 		if (user == null) {
 			resp.sendRedirect("login");
 			return;
 		}
-		
+
 		// get parameters
 		String type = req.getParameter("type");
-		Integer orderId = ParseUtil.String2Integer(req.getParameter("oid"), null);
+		Integer orderId = ParseUtil.String2Integer(req.getParameter("oid"),
+				null);
 		if (type != null) {
-			
+
 			if (type.equals("list")) {
 				list(req, resp);
 			} else if (type.equals("certain")) {
 				certain(req, resp);
 			} else if (orderId != null) {
-			
+
 				if (type.equals("show")) {
 					show(req, resp, orderId);
 				} else if (type.equals("comment")) {
 					comment(req, resp, orderId);
+				} else if (type.equals("shopOwnerShow")) {
+					shopOwnerShow(req, resp, orderId);
 				}
 			}
 		}
 	}
-	
-	private void comment(HttpServletRequest req, HttpServletResponse resp, Integer orderId) throws ServletException, IOException {
+
+	private void shopOwnerShow(HttpServletRequest req,
+			HttpServletResponse resp, Integer orderId) throws ServletException,
+			IOException {
+
+		User user = (User) req.getSession().getAttribute(
+				AttrName.SessionScope.USER);
+
+		Order order = orderService.findById(orderId);
+		if (order == null) {
+			return;
+		}
+
+		List<OrderInfo> orderInfos = orderService.getOrderInfo(orderId);
+		order.setOrderInfos(orderInfos);
 		
+		boolean exist = false;
+		for (int i = 0; i < orderInfos.size(); ++i) {
+
+			OrderInfo orderInfo = orderInfos.get(i);
+			
+			Product product = productService.getProductById(orderInfo
+					.getProductId());
+			orderInfo.setProduct(product);
+
+			Shop shop = shopService
+					.findById(orderInfo.getProduct().getShopId());
+			orderInfo.getProduct().setShop(shop);
+
+			if (shop.getOwner().equals(user.getUserId())) {
+				exist = true;
+			} else {
+				orderInfos.set(i, null);
+				continue;
+			}
+
+			Delivery delivery = deliveryService.select(
+					new Delivery(orderInfo.getDeliveryId(), null, null, null))
+					.get(0);
+			orderInfo.setDelivery(delivery);
+
+			//
+			orderInfo.setColor("");
+			orderInfo.setSize("/");
+			orderInfo.setProductUrl("productInfo?pid="
+					+ orderInfo.getProductId());
+		}
+		
+		if (!exist) {
+			return;
+		}
+
+		Address address = addressService.findById(order.getAddressId());
+		order.setAddress(address);
+		
+		order.setUserName(userService.findById(order.getUserId()).getName());
+
+		JSONObject jsonObject = JSONObject.fromObject(order);
+		resp.getWriter().write(jsonObject.toString());
+
+	}
+
+	private void comment(HttpServletRequest req, HttpServletResponse resp,
+			Integer orderId) throws ServletException, IOException {
+
 		show(req, resp, orderId);
 	}
 
-	private void show(HttpServletRequest req, HttpServletResponse resp, Integer orderId) throws ServletException, IOException {
+	private void show(HttpServletRequest req, HttpServletResponse resp,
+			Integer orderId) throws ServletException, IOException {
 
-		User user = (User) req.getSession().getAttribute(AttrName.SessionScope.USER);
-		
+		User user = (User) req.getSession().getAttribute(
+				AttrName.SessionScope.USER);
+
 		Order order = orderService.findById(orderId);
 		if (order == null || !order.getUserId().equals(user.getUserId())) {
 			return;
 		}
-		
+
 		Address address = addressService.findById(order.getAddressId());
 		order.setAddress(address);
-		
+
 		List<OrderInfo> orderInfos = orderService.getOrderInfo(orderId);
 		order.setOrderInfos(orderInfos);
 		for (OrderInfo orderInfo : orderInfos) {
-			
-			Product product = productService.getProductById(orderInfo.getProductId());
+
+			Product product = productService.getProductById(orderInfo
+					.getProductId());
 			orderInfo.setProduct(product);
-			
-			Delivery delivery = deliveryService.select(new Delivery(orderInfo.getDeliveryId(), null, null, null)).get(0);
+
+			Delivery delivery = deliveryService.select(
+					new Delivery(orderInfo.getDeliveryId(), null, null, null))
+					.get(0);
 			orderInfo.setDelivery(delivery);
-			
-			Shop shop = shopService.findById(orderInfo.getProduct().getShopId());
+
+			Shop shop = shopService
+					.findById(orderInfo.getProduct().getShopId());
 			orderInfo.getProduct().setShop(shop);
-			
+
 			//
 			orderInfo.setColor("");
 			orderInfo.setSize("/");
-			orderInfo.setProductUrl("productInfo?pid=" + orderInfo.getProductId());
+			orderInfo.setProductUrl("productInfo?pid="
+					+ orderInfo.getProductId());
 		}
-		
+
 		JSONObject jsonObject = JSONObject.fromObject(order);
 		resp.getWriter().write(jsonObject.toString());
 	}
@@ -115,32 +194,39 @@ public class OrderServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 	}
 
-	private void list(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
-		User user = (User) req.getSession().getAttribute(AttrName.SessionScope.USER);
-		
+	private void list(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+
+		User user = (User) req.getSession().getAttribute(
+				AttrName.SessionScope.USER);
+
 		List<Order> orders = orderService.getOrder(user, false);
 		for (Order order : orders) {
-			
-			List<OrderInfo> orderInfos = orderService.getOrderInfo(order.getOrderId());
+
+			List<OrderInfo> orderInfos = orderService.getOrderInfo(order
+					.getOrderId());
 			order.setOrderInfos(orderInfos);
 			for (OrderInfo orderInfo : orderInfos) {
-				
-				Product product = productService.getProductById(orderInfo.getProductId());
+
+				Product product = productService.getProductById(orderInfo
+						.getProductId());
 				orderInfo.setProduct(product);
-				
-				Delivery delivery = deliveryService.select(new Delivery(orderInfo.getDeliveryId(), null, null, null)).get(0);
+
+				Delivery delivery = deliveryService.select(
+						new Delivery(orderInfo.getDeliveryId(), null, null,
+								null)).get(0);
 				orderInfo.setDelivery(delivery);
-				
+
 				//
 				orderInfo.setColor("");
 				orderInfo.setSize("/");
-				orderInfo.setProductUrl("productInfo?pid=" + orderInfo.getProductId());
+				orderInfo.setProductUrl("productInfo?pid="
+						+ orderInfo.getProductId());
 			}
 		}
-		
+
 		Collections.reverse(orders);
-		
+
 		JSONArray jsonArray = JSONArray.fromObject(orders);
 		resp.getWriter().write(jsonArray.toString());
 	}
